395f5fb9f66e9b44b37f8496ae0f5a3d89bb09cf
altamash kalal <masai4last@gmail.com>
2023-10-14 08:31:35 +0530

    debug the code

--- PrimeOrNot.js
+++ PrimeOrNot.js
@@ -1,1 +1,16 @@
-
+function PrimeCheck(num){
+    let count=0;
+    for(let i=1;i<=num;i++){
+        if(num%i==0){
+            count==1
+        }
+    }
+    if (count==2){
+        console.log("Num is Prime")
+    }
+    else{
+        console.log("Not Prime")
+    }
+  
+  }
+  PrimeCheck(13);
